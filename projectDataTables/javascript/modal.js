dfa6aec5296ff170ec987842c7edcc1379987dbd
Firks, Andrew T <afirks@iu.edu>
2019-10-24 09:10:33 -0400

    added modal javascript

---
+++ projectDataTables/javascript/modal.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+// Get the modal
+var modal = document.getElementsByClassName("modal")[0];
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+$('.editBtn').click(function(){modal.style.display = "block";})
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+});
